cd04a7a1ab227136217358e63ce4056712cec117
Nithya <nithya.gopal@syncfusion.com>
2023-06-09 09:59:10 +0530

    312686-Remove-the-dashboard-listing-in-blazorwithJS-Sample

--- Models/EmbedClass.cs
+++ Models/EmbedClass.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -12,6 +13,30 @@ namespace BoldBIEmbedSample.Models
         public string embedQuerString { get; set; }
         [DataMember]
         public string dashboardServerApiUrl { get; set; }
+    }
+    public class Token
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("expires_in")]
+        public string ExpiresIn { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        public string LoginResult { get; set; }
+
+        public string LoginStatusInfo { get; set; }
+
+        [JsonProperty(".issued")]
+        public string Issued { get; set; }
+
+        [JsonProperty(".expires")]
+        public string Expires { get; set; }
     }
 
     public class EmbedDetails
